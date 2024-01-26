2ff7414506f6688c00f7f005a6b867da91e37e63
djsteinke <djsteinke@gmail.com>
2024-01-26 16:15:19 -0700

    requirements

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-import os
+import io
 import json
 from flask import Flask, render_template, Response, jsonify, request
-import cv2
-
+#import cv2
+from picamera2 import Picamera2
+from libcamera import controls
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)
+#camera = cv2.VideoCapture(0)
 setup = []
+camera = Picamera2()
+capture_config = camera.create_still_configuration()
+camera.start(show_preview=False)
+camera.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 0.0})
 
 
 def gen_frames():
     while True:
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+        data = io.BytesIO()
+        camera.capture_file(data, format='jpeg')
+        #success, frame = camera.read()  # read the camera frame
+        #if not success:
+        #    break
+        #else:
+        #    ret, buffer = cv2.imencode('.jpg', frame)
+        #    frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + data.read() + b'\r\n')  # concat frame one by one and show result
 
 
 @app.route('/')
@@ -74,5 +81,5 @@ def load_setup():
 
 if __name__ == '__main__':
     load_setup()
-    app.run(debug=True, port=31000)
+    app.run(debug=True, host="192.168.0.154", port=31000)
 

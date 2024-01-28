933c02b5094555271b1c85a8b4a48558e5a7e29b
Daniel Steinke <djsteinke@gmail.com>
2024-01-28 13:43:42 -0700

    cam

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
-from picamera2 import Picamera2, Preview
+import picamera2
 #from libcamera import controls
 from time import sleep
-import threading
-import flaskApp
-#import cv2
+from threading import Condition
+from flask import Flask, Response
+from flask_restful import Resource, Api
+import io
 
-#camera = cv2.VideoCapture(0)
-camera = Picamera2()
-config = camera.create_video_configuration(
-    main={"size": (640, 480)},
-    controls={"FrameDurationLimits": (15000, 15000)}, #this should be about 60fps
-    buffer_count=2
-    )
-camera.configure(config)
-camera.start()
-sleep(1) #time to let the camera start
+
+app = Flask(__name__)
+api = Api(app)
+
+class Camera:
+    def __init__(self):
+        self.camera = picamera2.Picamera2()
+        self.camera.configure(self.camera.create_video_configuration(main={"size": (640, 480)}))
+        self.streamOut = StreamingOutput()
+        self.camera.start()
+
+    def get_frame(self):
+        self.camera.start()
+        with self.streamOut.condition:
+            self.streamOut.condition.wait()
+            self.frame = self.streamOut.frame
+        return self.frame
+
+
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+
+
+# defines the function that generates our frames
+camera = Camera()
+
+
+def genFrames():
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+# defines the route that will access the video feed and call the feed function
+class VideoFeed(Resource):
+    def get(self):
+        return Response(genFrames(),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+api.add_resource(VideoFeed, '/cam')
 
 if __name__ == '__main__':
-    while True:
-        if camera is not None:
-            break
-        sleep(1)
-    #flaskApp.picam2 = camera
-    flaskApp.start_app()
+
+    app.run(debug=True, host="192.168.0.154", port=31000)
 
 

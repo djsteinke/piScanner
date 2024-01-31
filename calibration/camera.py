5401e4e886b4f99f7d4ccdb0d18c337ed641a154
djsteinke <djsteinke@gmail.com>
2024-01-31 13:36:46 -0700

    motor

--- calibration/camera.py
+++ calibration/camera.py
@@ -93,7 +93,7 @@ class CameraCalibration(object):
                 # cv2.waitKey(500)
 
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray_pic.shape[::-1], None, None)
-        new_camera_mtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+        new_camera_mtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (h, w), 1, (h, w))
 
         print('calibrationMatrixValues-mtx', cv2.calibrationMatrixValues(mtx, gray_pic.shape[::-1], 6.4512, 3.6288))
         print('calibrationMatrixValues-new_cam', cv2.calibrationMatrixValues(new_camera_mtx, gray_pic.shape[::-1], 6.4512, 3.6288))

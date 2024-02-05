e8d411da9a860491db5b5fe8510b70c0399736cd
djsteinke <djsteinke@gmail.com>
2024-02-05 10:02:26 -0700

    motor

--- accessories/camera.py
+++ accessories/camera.py
@@ -74,7 +74,7 @@ class Camera(object):
             self.picam2 = Picamera2()
         self.create_configs()
         self.picam2.start_preview(Preview.NULL)
-        self.picam2.align_configuration(self.configs['save'])
+        self.set_config('preview')
         self.picam2.start()
         sleep(1)
 

24c63e73463c56fffc9be201e66209618cc4f967
djsteinke <djsteinke@gmail.com>
2024-02-05 11:19:12 -0700

    motor

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ app = Flask(__name__)
 
 
 def gen_frames():
-    camera.set_focus_mm(scanner_config.camera.cz)
-    camera.set_config('save')
     while True:
         try:
             buffer = get_jpg_buffer(535, 964)
@@ -98,5 +96,6 @@ def load_setup():
 if __name__ == '__main__':
     scanner_config = ScannerConfiguration.load()
     camera.start()
+    camera.set_focus_mm(scanner_config.camera.cz)
     app.run(debug=False, host="192.168.0.154", port=3100)
 

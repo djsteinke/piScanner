849b72e5a1fab8f4765d7afc068a1127ee376303
djsteinke <djsteinke@gmail.com>
2024-02-08 14:19:11 -0700

    motor

--- parse_scan.py
+++ parse_scan.py
@@ -1,9 +1,12 @@
 import argparse
 import os.path
 import glob
+import cv2
 
 import scanner_paths
 from scan import ScanDetails
+from configuration.configuration import ScannerConfiguration
+from parser_util import *
 
 def output_asc_pointset(filename, points, pointset_type='xyz'):
     """
@@ -33,19 +36,26 @@ def output_asc_pointset(filename, points, pointset_type='xyz'):
 class ScanParser(object):
     def __init__(self, path):
         self.path = os.path.join(scanner_paths.scans_path, path)
+        self.last_xyz = []
+        self.first_xyz = []
+        self.details = None
+        self.config = ScannerConfiguration.load()
+        self.load_details()
 
-    def parse(self):
+    def load_details(self):
+        try:
+            self.details = ScanDetails.load(self.path)
+        except:
+            raise Exception()
+
+    def validate_path(self):
         if os.path.exists(self.path):
             print(f'parsing {self.path}')
         else:
             print(f'No scan found at {self.path}')
-            return
+            raise Exception()
 
-        try:
-            details = ScanDetails.load(self.path)
-        except Exception as e:
-            return
-
+    def parse(self):
         image_path = os.path.join(self.path, 'images')
         points = []
         right = glob.glob("%s/right*" % image_path.rstrip('/'))
@@ -79,6 +89,65 @@ class ScanParser(object):
         print("I: Writing pointcloud to %s" % filename)
         output_asc_pointset(filename, points, 'xyzcn')
 
+    def points_process_images(self, images, color=None, right=True):
+        points = []
+        # images = images[1:]
+        s = len(images)
+        xyz_all = []
+        for i, i_path in enumerate(images):
+            if i <= s:
+                if right:
+                    pic_num = i_path.split('right_')
+                else:
+                    pic_num = i_path.split('left_')
+                pic_num = int(pic_num[1].split('.')[0])
+                side = "RIGHT" if right else "LEFT"
+                img = cv2.imread(i_path, 0)
+                img_c = cv2.imread(i_path)
+                c = None
+                max_cols_c = True
+                if color is not None and i < len(color):
+                    # max_cols_c = False
+                    c = cv2.imread(color[i])
+                    c_gray = cv2.imread(color[i], 0)
+                    img = cv2.subtract(img, c_gray)
+
+                h, w = img.shape
+                """
+                # if we want to resize the images to parse quicker
+                if ratio > 1:
+                    h_tmp = int(h / ratio)
+                    w_tmp = int(w / ratio)
+                    img = cv2.resize(img, (w_tmp, h_tmp), interpolation=cv2.INTER_AREA)
+                    c = cv2.resize(c, (w_tmp, h_tmp), interpolation=cv2.INTER_AREA)
+                    h, w = img.shape
+                """
+                roi = [[0, 1080], [0, 1920]]
+                xy = points_max_cols(img_c, threshold=(60, 255), c=max_cols_c, roi=roi, right=right)
+                # xy = remove_noise(xy, w)
+
+                offset = pic_num * float(self.details.dps)
+
+                print("%s: %03d/%03d" % (side, pic_num + 1, s), round(offset, 1))
+                xyz = []
+                for x, y in xy:
+                    p = points_triangulate(self.config, (x, y), offset, color=c, right=right)
+                    r = math.sqrt(math.pow(p[0], 2) + math.pow(p[2], 2))
+                    # Remove points larger than the scan table
+                    if r < 110:
+                        xyz.append(p)
+
+                xyz_all.append([i, offset, xyz])
+
+        for i in range(0, len(xyz_all)):
+            if i == 0:
+                xyz = calculate_normal_vector(xyz_all[i][2], xyz_all[i + 1][2], right)
+            else:
+                xyz = calculate_normal_vector(xyz_all[i][2], xyz_all[i - 1][2], right)
+            xyz = [[x, y, z, r, g, b, xn, yn, zn] for x, y, z, r, g, b, xn, yn, zn, flip in xyz]
+            points.extend(xyz)
+        return points
+
 
 def main():
     # parse command line options
@@ -95,9 +164,11 @@ def main():
     if len(path) == 0:
         print('-p / --path is required.')
         return
-
-    parser = ScanParser(path)
-    parser.parse()
+    try:
+        parser = ScanParser(path)
+        parser.parse()
+    except:
+        print("Parse failed.")
 
 
 if __name__ == "__main__":
---
+++ parser_util.py
@@ -0,0 +1,143 @@
+import cv2
+import numpy as np
+import math
+from configuration.configuration import ScannerConfiguration
+
+
+def vector_normal(p1, p2, p3):
+    v1 = [(p1[0]-p2[0]), (p1[1]-p2[1]), (p1[2]-p2[2])]
+    v2 = [(p3[0]-p2[0]), (p3[1]-p2[1]), (p3[2]-p2[2])]
+    #v1, v2 = vector_angle(v1, v2, offset)
+    n = [(v1[1]*v2[2]-v1[2]*v2[1]), -1*(v1[0]*v2[2]-v1[2]*v2[0]), (v1[0]*v2[1]-v1[1]*v2[0])]
+    if p2[9]:
+        n = [n[0]*-1, n[1]*-1, n[2]*-1]
+    v1m = math.sqrt(math.pow(v1[0], 2) + math.pow(v1[1], 2) + math.pow(v1[2], 2))
+    v2m = math.sqrt(math.pow(v2[0], 2) + math.pow(v2[1], 2) + math.pow(v2[2], 2))
+    vm = v1m * v2m
+    if vm == 0:
+        return [0.0, 0.0, 0.0]
+    else:
+        return [n[0]/vm, n[1]/vm, n[2]/vm]
+
+
+def calculate_normal_vector(xyz, xyz2, flip=False):
+    length = len(xyz)
+    l2 = len(xyz2)
+    r = 8
+    for i in range(0, length-r, 1):
+        if flip:
+            p1 = xyz2[i if i < l2 else l2-1]
+        else:
+            p1 = xyz[i]
+        p2 = xyz[i+r]
+        if flip:
+            p3 = xyz[i]
+        else:
+            p3 = xyz2[i if i < l2 else l2-1]
+        n = vector_normal(p2, p1, p3)
+
+        xyz[i][6] = n[0]
+        xyz[i][7] = n[1]
+        xyz[i][8] = n[2]
+    return xyz
+
+
+def points_triangulate(config: ScannerConfiguration, points, offset, color=None, right=True):
+    left_offset = 0
+    if right:
+        cam_degree = config.right_laser.angle
+    else:
+        cam_degree = -config.left_laser.angle
+        left_offset = config.right_laser.angle + config.left_laser.angle
+
+    px, py = points
+
+    bgr = [147, 201, 3]
+    if color is not None:
+        bgr = color[round(py), round(px)]
+
+    cx = config.camera.cx
+    cy = config.camera.cy
+    if right:
+        calc_x = (float(px)-cx)/config.camera.f
+        flip = calc_x < 0.0
+    else:
+        calc_x = (cx-float(px))/config.camera.f
+        flip = calc_x > 0.0
+
+    if right:
+        calc_z = -calc_x / math.tan(math.radians(cam_degree))
+    else:
+        calc_z = calc_x / math.tan(math.radians(cam_degree))
+
+    calc_y = (float(py)-cy)/config.camera.f
+
+    if not right:
+        offset += left_offset
+    angle = math.radians(offset)
+
+    orig_x = calc_x
+    orig_z = calc_z
+    calc_x = orig_x*math.cos(angle) - orig_z*math.sin(angle)
+    calc_z = orig_x*math.sin(angle) + orig_z*math.cos(angle)
+
+    return [
+        calc_x,
+        calc_y,
+        calc_z,
+        bgr[2], bgr[1], bgr[0],
+        0.0, 0.0, 0.0, flip
+    ]
+
+
+def points_max_cols(img, threshold=(60, 255), c=False, roi=None, right=True):
+
+    lower = np.array([30, 20, 60])
+    upper = np.array([175, 125, 255])
+    if c:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        t_min, t_max = threshold
+        ret, bin_img = cv2.threshold(gray, t_min, t_max, cv2.THRESH_BINARY)
+        # contour, hierarchy = cv2.findContours(bin_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        h, w = bin_img.shape
+    else:
+        bin_img = cv2.inRange(img, lower, upper)
+        h, w, _ = img.shape
+
+    """
+    if roi is None:
+        x_roi, y_roi = get_roi_by_img(img, 1)
+    else:
+    """
+    x_roi = roi[0]
+    y_roi = roi[1]
+
+    s = 1
+    if right:
+        s = -1
+        x_roi = [x_roi[1]-1, x_roi[0]-1]
+
+    xy = list()
+
+    avg = []
+    for i in range(0, h):
+        avg.append([0, 0])
+
+    for y in range(y_roi[0], y_roi[1], 4):
+        found = False
+        for x in range(x_roi[0], x_roi[1], s):
+            if bin_img[y][x] > 0:
+                avg[y][0] += x
+                avg[y][1] += 1
+                found = True
+            else:
+                if found:
+                    break
+
+    for i in range(0, len(avg)):
+        a = avg[i]
+        if a[1] > 0:
+            x_avg = int(round(a[0]/a[1], 0))
+            xy.append((x_avg, i))
+
+    return xy
